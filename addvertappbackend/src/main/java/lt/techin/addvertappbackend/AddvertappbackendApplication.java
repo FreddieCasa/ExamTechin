e59bd900e7f0eb2e346dcef8acfd702712c42b3f
Lukas Rutkauskas <lukas.rutkauskas@stud.techin.lt>
2024-06-27 10:23:39 +0300

    init

---
+++ addvertappbackend/src/main/java/lt/techin/addvertappbackend/AddvertappbackendApplication.java
@@ -0,0 +1,13 @@
+package lt.techin.addvertappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddvertappbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddvertappbackendApplication.class, args);
+	}
+
+}
