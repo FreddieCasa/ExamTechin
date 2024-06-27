8bc9ad58126b74976b576cdbf0f2190873bb71de
GedvinasP <lukas.rutkauskas@stud.techin.lt>
2024-06-27 12:52:45 +0300

    add mapping

--- backend/src/main/java/lt/techin/backend/model/AdvertCategory.java
+++ backend/src/main/java/lt/techin/backend/model/AdvertCategory.java
@@ -1,8 +1,14 @@
 package lt.techin.backend.model;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+
 
 @Entity
 public class AdvertCategory {
@@ -10,17 +16,32 @@ public class AdvertCategory {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
-
+ 
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Advert> adverts;
+ 
+    // Getters and setters
     public Long getId() {
         return id;
     }
+ 
     public void setId(Long id) {
-        this.id = id;
+this.id = id;
     }
+ 
     public String getTitle() {
         return title;
     }
+ 
     public void setTitle(String title) {
         this.title = title;
+    }
+ 
+    public List<Advert> getAdverts() {
+        return adverts;
+    }
+ 
+    public void setAdverts(List<Advert> adverts) {
+        this.adverts = adverts;
     }
 }
